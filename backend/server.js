4214d89b86916a1b2d6defcdb0612af9c0139b02
SuyogK <suyogk@AgainNewUbuntu.myguest.virtualbox.org>
2024-12-02 19:11:13 +0530

    connection success

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,12 @@ const PORT = 3000;
 
 // Enable CORS
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 // Mock data
 const users = [
